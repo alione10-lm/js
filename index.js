64c023832d35779aa3166e48acfcbc560af1f488
alione10-lm <aliouaneoussama89@gmail.com>
2025-11-11 14:55:49 +0100

    add prompt in each function

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const numero = prompt("votre numero de telephone => ");
 console.log(nom, prenom, age, sexe, numero);
 
 // challenge 2 :
-const ConvertoCelToFah = (Cel) => {
+const ConvertoCelToFah = () => {
+  const Cel = prompt("enter temp : ");
   return Cel * 1.8 + 32;
 };
 
@@ -20,7 +21,10 @@ let c = b;
 var a = c;
 
 // challenge  4
-const Operations = (a, b) => {
+const Operations = () => {
+  const a = prompt("enter number a : ");
+  const b = prompt("enter number b : ");
+
   console.log(
     ` a+b : ${a + b} \n , a-b : ${a - b} \n , a*b : ${a * b} , a/b : ${
       a / b
@@ -29,33 +33,46 @@ const Operations = (a, b) => {
 };
 
 // challenge 5
-const SumMoy = (a, b, c, d) => {
+const SumMoy = () => {
+  const a = prompt("enter number a : ");
+  const b = prompt("enter number b : ");
+  const c = prompt("enter number c : ");
+  const d = prompt("enter number d : ");
   const somme = a + b + c + d;
   console.log(`la somme : ${somme} \n la moyenne : ${somme / 4}`);
 };
 
 // challenge  6
-const TempsDeTrajet = (vitesse, distance) => {
+const TempsDeTrajet = () => {
+  const distance = prompt("enter distance : ");
+  const vitesse = prompt("enter vitesse : ");
   return distance / vitesse;
 };
 
 // challenge 7
-const Circonference = (rayon) => {
+const Circonference = () => {
+  const rayon = prompt("enter rayon: ");
   return 2 * Math.PI * rayon;
 };
 
 // challenge 8
-const ReverseInt = (num) => {
+const ReverseInt = () => {
+  const num = prompt("enter a number contains tree chars : ");
+
   const arr = [num];
   return Number(arr.join("").split("").reverse().join(""));
 };
 
 // challenge 9
 const KmToMile = (km) => {
+  const km = prompt("enter a distance in km: ");
+
   return km / 1.609;
 };
 
 // challenge 10
 const MoneyConverter = (euro) => {
+  const euro = prompt("enter an amount of euro ");
+
   return euro / 10.61;
 };
